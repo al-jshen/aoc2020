6218e04a4e4516d1272b5411272b14d3e4bf9186
Jeff Shen <jshen2014@hotmail.com>
2020-12-03 15:37:56 -0500

    finished day 2 part 2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -11,7 +11,7 @@ struct PassPolicy {
 }
 
 fn main() {
-    println!("{}", pt1("./input.txt"));
+    println!("{}", pt2("./input.txt"));
 }
 
 fn pt1(fp: &str) -> u32 {
@@ -21,6 +21,22 @@ fn pt1(fp: &str) -> u32 {
             let y: PassPolicy = x.parse().unwrap();
             if y.low <= y.pass.matches(y.letter).count()
                 && y.pass.matches(y.letter).count() <= y.high
+            {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        .sum()
+}
+
+fn pt2(fp: &str) -> u32 {
+    let inp = read_to_string(fp).unwrap();
+    inp.lines()
+        .map(|x| {
+            let y: PassPolicy = x.parse().unwrap();
+            if (y.pass.chars().nth(y.low - 1).unwrap() == y.letter)
+                ^ (y.pass.chars().nth(y.high - 1).unwrap() == y.letter)
             {
                 return 1;
             } else {
